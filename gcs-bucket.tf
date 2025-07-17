d840fe9f6538e9758e34ebf627739175622cb9c2
Alex Cowan <3968473+alextcowan@users.noreply.github.com>
2025-07-17 04:42:05 +0000

    Cleanup PAC generation

--- gcs-bucket.tf
+++ gcs-bucket.tf
@@ -3,6 +3,17 @@
 ## Create public Google Cloud Storage Bucket and upload PAC file
 ##
 #############################################################################
+
+locals {
+  all_hostnames = flatten([
+    for app in values(var.applications) : app.hostnames
+  ])
+
+  pac_sites_list_string = join(", ", [
+    for hostname in local.all_hostnames : "\"${hostname}\""
+  ])
+}
+
 resource "google_storage_bucket" "public_bucket" {
   name     = var.bucket_name
   location = var.bucket_region
@@ -16,9 +27,15 @@ resource "google_storage_bucket_iam_member" "public_access" {
 }
 
 resource "google_storage_bucket_object" "pac_object" {
-  name = basename(var.pac_file_path)
+  name = "sgw-pac.js"
   bucket = google_storage_bucket.public_bucket.name
-  source = var.pac_file_path
+  
+  content = templatefile("${path.module}/sgw-pac.js.tpl", {
+    sites_list = local.pac_sites_list_string
+  })
+
+  content_type = "text/javascript"
+
   cache_control = "no-cache"
   depends_on = [
     google_storage_bucket_iam_member.public_access
